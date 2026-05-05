cf522c92fcdde528224ba6c5aa78125c3ab093e9
DevilxxxLord <tian.i@bk.ru>
2026-05-05 09:27:29 +0300

    Обновил добавив несколько пакетов

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"errors"
-	"time"
-)
-
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-var BinList = []Bin{}
-
 func main() {
-
-}
-
-func createdBin(b *Bin) (c *Bin, err error) {
-	if b.id == "" {
-		return nil, errors.New("INVALID_ID")
-	}
-	if b.name == "" {
-		return nil, errors.New("INVALID_NAME")
-	}
-	return c, nil
-}
-
-func createdBinList([]Bin) {
 
 }
